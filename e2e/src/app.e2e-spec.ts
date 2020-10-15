828f7be4e5c0e9f4c06417e5696f0aa35c6a965d
Guillermo Galindo <guillermogs19@gmail.com>
2020-10-15 02:13:35 +0200

    main

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-store!');
+    expect(page.getTitleText()).toEqual('Welcome to cncienta!');
   });
 
   afterEach(async () => {
