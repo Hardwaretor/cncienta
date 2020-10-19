ab8c0f09fe81d5f74693057fc993771792faed14
Guillermo Galindo <guillermogs19@gmail.com>
2020-10-19 11:46:00 +0200

    header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,28 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, Input } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { Component, OnInit, } from '@angular/core';
 import { Counter } from '../_models';
 import { AccountService } from '../_services';
+import { User } from '../_models';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent implements OnInit {
 
 
   counter: Counter;
-
-  @Input() Counter: Observable<any>;
+  user: User;
 
   constructor(
 
     private accountService: AccountService,
 
-
 ) {
 
   
   this.counter = this.accountService.counterValue;
-
+  this.user = this.accountService.userValue;
 
   }
  
