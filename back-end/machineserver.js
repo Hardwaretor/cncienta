7e0ef5bfca22e9ab27a6d83d853e04fb1d931616
Guillermo Galindo <guillermogs19@gmail.com>
2020-10-21 21:59:16 +0200

    counter

--- back-end/machineserver.js
+++ back-end/machineserver.js
@@ -6,6 +6,25 @@ var config = require('./config/config.json');
 const app = express();
 const { Boards, Led } = require("johnny-five");
 
+var socket = {};
+var serialports = [];
+var listenSerialport = null;
+
+app.use(express.static('./machine.html'));
+app.use(require('cookie-parser')());
+app.use(require('cookie-session')({
+	secret: config.secret
+	
+}));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var sp = require('serialport');
+
+var router = express.Router();
+
 const SerialPort = require('serialport');
 // Promise approach
 SerialPort.list().then(ports => {
@@ -21,25 +40,6 @@ var board = five.Board();
 
 board.on('ready', function () {
 
-var pubnub = require('pubnub').init({
-publish_key: 'YOUR_PUBLISH_KEY',
-        subscribe_key: 'YOUR_SUBSCRIBE_KEY'
-        
-});
-
-
-
-function setLedColor(receivedObj) {
-console.log("receivedObj :", receivedObj);
-if (receivedObj.led_status === 1) {
-console.log("ON");
-led8.on();
-}
-if (receivedObj.led_status === 0) {
-console.log("OFF");
-led8.off();
-}
-}
 
 const boards = new Boards(["A", "B"]);
 
@@ -73,18 +73,70 @@ boards.on("ready", () => {
   });
 });
 
-function initLedColor() {
-   led8 = new five.Led(8);
-   led8.off();
-   console.log("Arduino server is running now.");
-   console.log("Initilization, first call function");
-  }
 
+
+  router.post('/api/serialport',function(req,res){
+
+    if(listenSerialport == null) {
+      var nameOfPort = req.body.name;
+      serialports.forEach((item)=> {
+        if(item === nameOfPort){
+          listenSerialport = new sp.SerialPort(nameOfPort,{baudRate:19200,parser: sp.parsers.readline('\n')});
+          listenSerialport.on('open', function () {
+            console.log("OPEN port" + nameOfPort);
   
+          });
+  
+          listenSerialport.on('data', function (data) {
+            try{
+              var  serialData = JSON.parse("" +data);
+              socket.emit('data',serialData);
+  
+            } catch (SyntaxError){
+              console.log("Error");
+            }
+  
+                  });
+                  
+                  
+        }
+      });
+      res.send({status:"OK"});
+    } else if(!res.finished)
+      res.send({status:"ERROR"});
+  
+  
+  });
+  
+  sp.list(function(err, ports) {
+    ports.forEach((item)=>{
+      serialports.push(item.comName);
+    });
+    console.log(serialports);
+  });
+  
+  sp.list().then(ports => {
+    ports.forEach((item)=>{
+      serialports.push(item.comName);
+    });
+    console.log(serialports);
+  });
+  
+  app.use(router);
+  
+  sp.list(function(err, ports) {
+    ports.forEach((item)=>{
+      serialports.push(item.comName);
+    });
+    console.log(serialports);
+  });
 
 
 
 });
+
+
+
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -94,8 +146,26 @@ app.get('*', (req, res) => {
 const port = 4002;
 app.set('port', port);
 const server = http.createServer(app);
-server.listen(port, () => console.log(`CNCienta.com Machine Controller on port:${port}`));
+
+server.listen(port, function () {
+	var host = server.address().address;
+  var port = server.address().port;
+  () => console.log(`CNCienta.com Machine Controller on port:${host,port}`)});
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 
+var io = require('socket.io')(server);
+
+//io.set('transports', [
+//	'websocket'
+//	, 'flashsocket'
+//	, 'htmlfile'
+//	, 'xhr-polling'
+//	, 'jsonp-polling'
+//]);
+io.on('connection', function (sock) {
+	socket = sock;
+	socket.emit('serialports',serialports);
+	console.log('connection');
+});
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,9 +14,6 @@ import { Product } from '../_models'
 export class ProductListComponent implements OnInit {
 
   products = null;
-  inCart = false;
-
-  
 
   @Input() product: Product;
 
@@ -27,17 +24,21 @@ export class ProductListComponent implements OnInit {
     var newProduct = {cart};
     oldProducts.push(newProduct);
     localStorage.setItem('cart', JSON.stringify(oldProducts));
+
     var counter = JSON.parse(localStorage.cart).length
-    localStorage.setItem('counter', JSON.stringify(counter));
+    localStorage.counter = JSON.stringify(counter)
 
      }
     
 
-  removeToCart(cart: string ){
+  removeToCart(){
 
-  localStorage.removeItem('cart');
-  this.inCart = false;
+    const cart = JSON.parse(localStorage.cart ?? "[]")
+    localStorage.cart = JSON.stringify(cart.slice(0, -1))
 
+    const counter = JSON.parse(localStorage.counter ?? "[]")
+    localStorage.counter = JSON.stringify(counter-1)
+    
 }
 
   constructor(private accountService: AccountService) { 
